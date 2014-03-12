81c0159dafaff1fca1ef57f8e689204ff9a29102
Ignacio Bona <igbopie@gmail.com>
2014-03-12 19:23:00 +0100

    first commit

---
+++ src/main/java/com/example/work_seemmockup1/seemmockup1/Reply.java
@@ -0,0 +1,16 @@
+package com.example.work_seemmockup1.seemmockup1;
+
+import android.graphics.Bitmap;
+import android.net.Uri;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by igbopie on 12/03/14.
+ */
+public class Reply {
+    public Uri imageUri;
+    public Bitmap imageBitmap;
+    public List<Reply> replyList = new ArrayList<Reply>();
+}
